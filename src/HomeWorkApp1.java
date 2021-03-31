d1a23325c724009c1ed25697dbd9e579be54eed3
иван <expert-film@mail.ru>
2021-03-31 22:14:29 +0300

    выполнил второе д/з

--- src/HomeWorkApp1.java
+++ src/HomeWorkApp1.java
@@ -1,43 +1,61 @@
 public class HomeWorkApp1 {
-    public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-    }
-
-    public static void printThreeWords() {
-        System.out.println("_Orange");
-        System.out.println("_Banana");
-        System.out.println("_Apple");
-    }
-
-    public static void checkSumSign() {
-        int a = 5;
-        int b = 3;
-        System.out.println(a + b);
-        System.out.println("Сумма положительная");
-    }
-
-    public static void printColor() {
-        int value = 34;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (0 < value && value <= 100) {
-            System.out.println("Желтый");
-        } else if (100 < value) {
-            System.out.println("Зелёный");
-        }
-    }
-    public static void compareNumbers()
+    public static void main(String[] args)
     {
 
-        int a = 17;
-        int b = 2;
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else if (a < b) {
-            System.out.println("a < b");
+        System.out.println();
+        int a = 0;
+        int b = 1;
+        boolean isBetween = isSumBetween10And20(a, b);
+System.out.println(isBetween);
+
+        System.out.println();
+        printSignOf(2);
+
+        System.out.println();
+        boolean isBad = isBad (-4);
+        System.out.println(isBad);
+
+        System.out.println();
+        print("Let it be....", 8);
+
+        System.out.println("Задание 5");
+        boolean isHappy = isHappyYear(2021);
+        System.out.println(isHappy);
+
+    }
+
+
+    public static boolean isSumBetween10And20(int left, int right)
+    {
+        int sum = left + right;
+        return 10 < sum && sum <= 20;
+    }
+
+    public static void printSignOf(int number)
+    {
+        if (number >= 0) {
+            System.out.println("Положительное число");
+        } else {
+            System.out.println("Отрицательное число");
         }
     }
+
+    public static boolean isBad(int number)
+    {
+        return number < 0;
+    }
+
+    public static void print(String message,int count)
+    {
+        for (int i=0; i < count; i++)
+        {
+            System.out.println(message);
+        }
+    }
+public static boolean isHappyYear(int year)
+{
+    return (year % 4 == 0 && year % 100 !=0) || year % 400 == 0;
 }
+}
+
+
